d737a0c76fe5f2a7a145074ebdff158140957f52
Nick Lanese <nlanese21@gmail.com>
2020-08-20 01:32:51 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,16 @@
 class BankAccount
 
+  def initialize(inputName)
+    @name = inputName
+    @balance = 1000
+  end
+
+  def name=(inputName)
+    @name = inputName
+  end
+
+  def name 
+    @name
+  end
+
 end
