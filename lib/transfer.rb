73959a07d7dc073da106c19f3af05b940abbebe0
Nick Lanese <nlanese21@gmail.com>
2020-08-20 23:33:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
 
   def execute_transaction
     if (@status = "pending")
-      if (self.valid?)
+      if (self.valid? && (@sender.balance - @amount >= 0))
         @sender.balance=(@sender.balance - @amount)
         @receiver.balance=(@receiver.balance + @amount)
         @status = "complete"
